25a8d378a5c3f3262531784061960f8990ddd989
mingren.chen16 <mingren.chen16@gmail.com>
2019-05-22 09:46:17 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 首頁
 Route::get('/', function () {
     return view('welcome');
+    Route::group(['prefix' =>'user'], function(){
+        // User 驗證
+        Route::group(['prefix' => 'auth'], function(){
+            // User 註冊頁面
+            Route::get('/sign-up', 'UserAuthController@signUpPage');
+
+            // User 註冊資料處理
+            Route::post('/sign-up', 'UserAuthController@signUpProcess');
+
+            // User 登入頁面
+            Route::get('/sign-in', 'UserAuthController@signInPage');
+
+            // User 登入處理
+            Route::post('/sign-in', 'UserAuthController@signInProcess');
+
+            // User 登出
+            Route::get('/sign-out', 'UserAuthController@signOut');
+        });
+        
+        // User 個資相關
+        Route::group(['prefix' => 'profile'], function(){
+            // User 個資檢視
+            Route::get('/read', "UserProfileController@readProfile");
+
+            // User 個資修改
+            Route::get('/modify', "UserProfileController@modifyProfile");
+        });
+    });
 });
