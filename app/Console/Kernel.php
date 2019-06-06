66b6ff91c8e4d73c98ce8540bf299a2dac211747
mingren.chen16 <mingren.chen16@gmail.com>
2019-06-06 16:26:36 +0800

    modify schedule into proceeding every day morning 9am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('shop:sendAutoMailOfAskingForDebt')
-                  ->everyMinute();
+                  ->dailyAt('09:00');
     }
 
     /**
